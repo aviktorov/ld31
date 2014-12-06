57d3b5c66e4571a70e483d3194361a7b44e07a96
Andrey Viktorov <aviktorov@fatestudio.org>
2014-12-06 19:38:18 +0600

    Rename

---
+++ Assets/HeroTemp/Scripts/Hero.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ */
+[RequireComponent(typeof(Rigidbody))]
+public class Hero : MonoBehaviour {
+	
+	public float speed = 5.0f;
+	
+	private Rigidbody cachedBody;
+	
+	private void Awake() {
+		cachedBody = GetComponent<Rigidbody>();
+	}
+	
+	private Vector2 GetMovementVector() {
+		Vector2 direction = Vector2.zero;
+		
+		direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+		if(direction.sqrMagnitude > 1.0f) direction.Normalize();
+		
+		return direction;
+	}
+	
+	private void Update() {
+		if(cachedBody == null) return;
+		
+		Vector2 movement = GetMovementVector() * speed;
+		Vector3 velocity = cachedBody.velocity;
+		
+		velocity.x = movement.x;
+		velocity.z = movement.y;
+		
+		cachedBody.velocity = velocity;
+	}
+	
+	private void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.tag != "Exit") return;
+		
+		Destroy(this.gameObject);
+		UI.instance.gameOver = true;
+	}
+}
---
+++ Assets/HeroTemp/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoSingleton<UI> {
+	
+	public bool gameOver = false;
+	
+	private void OnGUI() {
+		if(!gameOver) return;
+		
+		Rect rect = new Rect((Screen.width - 200) / 2,(Screen.height - 20) / 2,200,20);
+		rect.y -= 30;
+		
+		GUI.Label(rect,"You loose!");
+		rect.y += 30;
+		
+		if(GUI.Button(rect,"Restart")) {
+			Application.LoadLevel("HeroTemp");
+		}
+	}
+}
